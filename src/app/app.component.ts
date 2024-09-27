85342db92ce985dc5109ace7a320d5ae2c11fa6e
PluralSight 3 <pluralsight-learning-3@cybsec.it>
2024-09-27 17:13:08 +0200

    update app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  destinatario = '';
+
+  public persona = "";
+  public destinatario = '';
   private url = "https://mvvm-mvvm.azuremicroservices.io/api/persone"; //azure spring app 
   // private url = "https://mvvm.azurewebsites.net/api/HttpTrigger1?code=ryjcl40cNXh43zJzmCiKOx5Cq9uSV4MxdZ4fpypf5nAMAzFu1dx6Gw%3D%3D"; //azure function 
 
+  public list: string[] = [];
+
   public constructor(private readonly http: HttpClient){}
+
+  add() {
+    this.list.push(this.persona);
+    this.persona = "";
+    
+  }
 
   fuckOff() {
     this.http.post(this.url, {data: ["Domenico", "Mancino"]} ).subscribe((res: any) => {
-      this.destinatario = res.res.toUpperCase();
+      this.destinatario = res.data.toUpperCase();
     })
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
+  
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(CommonModule)
+    importProvidersFrom(CommonModule, FormsModule)
   ],
 };
