3b5451488c1d5ceed546029804f0426e86e1a285
PluralSight 3 <pluralsight-learning-3@cybsec.it>
2024-09-27 16:53:34 +0200

    update url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   destinatario = '';
-  private url = "https://mvvm.azurewebsites.net/api/HttpTrigger1?code=ryjcl40cNXh43zJzmCiKOx5Cq9uSV4MxdZ4fpypf5nAMAzFu1dx6Gw%3D%3D";
+  private url = "https://mvvm-mvvm.azuremicroservices.io/api/persone"; //azure spring app 
+  // private url = "https://mvvm.azurewebsites.net/api/HttpTrigger1?code=ryjcl40cNXh43zJzmCiKOx5Cq9uSV4MxdZ4fpypf5nAMAzFu1dx6Gw%3D%3D"; //azure function 
 
   public constructor(private readonly http: HttpClient){}
 
   fuckOff() {
-    this.http.post(this.url, {name: "Domenico"} ).subscribe((res: any) => {
+    this.http.post(this.url, {data: ["Domenico", "Mancino"]} ).subscribe((res: any) => {
       this.destinatario = res.res.toUpperCase();
     })
   }
